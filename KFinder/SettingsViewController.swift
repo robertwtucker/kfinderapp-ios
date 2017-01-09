5a5d81978e78a9c9517146c9fe49889a17cbe853
Robert Tucker <robertwtucker@gmail.com>
2017-01-08 20:08:09 -0500

    Fixes missing implementation

--- KFinder/SettingsViewController.swift
+++ KFinder/SettingsViewController.swift
@@ -1,9 +1,26 @@
-//
-//  SettingsViewController.swift
-//  KFinder
-//
-//  Created by Robert Tucker on 1/8/17.
-//  Copyright © 2017 Robert Tucker. All rights reserved.
-//
+/**
+ * Copyright 2017 Robert Tucker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("SetingsVC loaded")
+    }
+}
+
+extension SettingsViewController: StoryboardIdentifiable { }
