0fb86d9531274d28dbaeb87ec12b80799394458f
Robert Tucker <robertwtucker@gmail.com>
2023-08-25 14:25:19 -0400

    chore: initial commit to source control (rewrite)

---
+++ KFinder/KFinderApp.swift
@@ -0,0 +1,15 @@
+//
+// SPDX-FileCopyrightText: 2016-2023 Robert Tucker
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+@main
+struct KFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
