95b050a7a83949cfc00019b1fb5aabe35a023743
Robert Tucker <robertwtucker@gmail.com>
2023-10-02 21:51:33 -0400

    refactor: consolidated target views

--- KFinder/Settings/VitaminKTargetView.swift
+++ KFinder/Settings/VitaminKTargetView.swift
@@ -6,19 +6,53 @@
 import SwiftUI
 
 struct VitaminKTargetView: View {
-  @AppStorage(StorageKeys.kTarget.rawValue) private var kTarget = 120
+  enum Field: Hashable {
+    case target
+  }
+  
+  @AppStorage(StorageKeys.kTarget.rawValue) private var kTarget: Double = 120
+  @State private var showingVitaminKInfo = false
+  @FocusState private var focusedField: Field?
   
   var body: some View {
-    HStack {
-      Label("settings.ktarget", systemImage: "target")
-      Spacer()
-      Text("\(kTarget) µg")
+    Form {
+      valueSection
+      infoSection
+      .defaultFocus($focusedField, .target)
+    }
+    .navigationTitle("settings.ktarget.title")
+  }
+  
+  private var valueSection: some View {
+    Section(header: Text("settings.ktarget.measure")) {
+      TextField("settings.ktarget.value", value: $kTarget, format: .number)
+        .multilineTextAlignment(.trailing)
+        .keyboardType(.numbersAndPunctuation)
+        .focused($focusedField, equals: .target)
+    }
+  }
+  
+  private var infoSection: some View {
+    let info: LocalizedStringKey = "settings.ktarget.info"
+    let footnote: LocalizedStringKey = "settings.ktarget.footnote"
+    
+    return Section {
+      VStack(spacing: 16) {
+        Label("settings.ktarget", systemImage: "info.circle.fill")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.accentColor)
+          .labelStyle(.iconOnly)
+        VStack(alignment: .leading, spacing: 32) {
+          Text(info)
+          Text(footnote)
+            .font(.footnote)
+        }
+      }
     }
   }
 }
 
-struct VitaminKTargetView_Previews: PreviewProvider {
-  static var previews: some View {
-    VitaminKTargetView()
-  }
+#Preview {
+  VitaminKTargetView()
 }
