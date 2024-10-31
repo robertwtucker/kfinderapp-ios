a66b4938a06e08ab6c6da3ae07e08dc9f197c8bf
Robert Tucker <robertwtucker@gmail.com>
2024-10-30 20:40:56 -0400

    refactor: now status view, stub to test notifications

---
+++ KFinder/Home/HomeStatusView.swift
@@ -0,0 +1,45 @@
+//
+// SPDX-FileCopyrightText: 2016-2024 Robert Tucker
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+struct HomeStatusView: View {
+  @Environment(\.colorScheme) private var colorScheme
+  @State var items: [String] = []  // (Notifications?)
+//  @State var items = ["Alert1", "Alert2"]
+
+  var body: some View {
+    GeometryReader { geometry in
+      VStack {
+        HStack {
+          Text("Next scheduled test...")
+            .padding(.horizontal, 8)
+            .foregroundStyle(colorScheme == .light ? .white : .black)
+            .frame(maxWidth: .infinity, maxHeight: 200)
+          Spacer()
+        }.background(RoundedRectangle(cornerRadius: 16))
+        ScrollView(Axis.Set.horizontal, showsIndicators: true) {
+          HStack {
+            ForEach(items, id: \.self) { item in
+              Text(item)
+                .padding(.horizontal, 8)
+                .foregroundStyle(colorScheme == .light ? .white : .black)
+                .frame(
+                  width: items.count > 1
+                  ? geometry.size.width - 24 : geometry.size.width - 8,
+                  height: 150
+                )
+              Spacer()
+            }.background(RoundedRectangle(cornerRadius: 16))
+          }
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  HomeStatusView()
+}
--- KFinder/Home/HomeTab.swift
+++ KFinder/Home/HomeTab.swift
@@ -3,40 +3,40 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import SwiftUI
 import Models
-import SwiftData
 import OSLog
+import SwiftData
+import SwiftUI
 
 struct HomeTab: View {
   @Environment(\.colorScheme) private var colorScheme
-  
+
   @Query(sort: \FoodItem.dateUpdated, order: .reverse)
   var foods: [FoodItem]
-  
+
   private let logger = Logger(
     subsystem: Bundle.main.bundleIdentifier!,
     category: String(describing: FoodsTab.self))
-  
+
   var body: some View {
     NavigationStack {
-        VStack(alignment: .leading, spacing: 8) {
-          HStack {
-            VStack(alignment: .leading, spacing: 2) {
-                Text("Hey there!")
-                  .font(.footnote)
-                Text("Here's what's up...")
-            }
-            Spacer()
-            Color.primary.frame(width: 50, height: 50)
-              .clipShape(.circle)
+      VStack(alignment: .leading, spacing: 8) {
+        HStack {
+          VStack(alignment: .leading, spacing: 2) {
+            Text("Hey there!")
+              .font(.footnote)
+            Text("Here's what's up...")
           }
-          TestingStatusView()
-          RecentFoodsListView()
           Spacer()
+          Color.primary.frame(width: 50, height: 50)
+            .clipShape(.circle)
         }
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        HomeStatusView()
+        RecentFoodsListView()
+        Spacer()
       }
+      .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+    }
   }
 }
 
