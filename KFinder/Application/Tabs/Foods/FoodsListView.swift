86c9cb020ba593b0c557556ce865b3785a680c13
Robert Tucker <robertwtucker@gmail.com>
2024-10-31 23:55:11 -0400

    refactor: consolidate cell view with list

--- KFinder/Application/Tabs/Foods/FoodsListView.swift
+++ KFinder/Application/Tabs/Foods/FoodsListView.swift
@@ -3,12 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import SwiftUI
 import Models
+import Services
+import SwiftUI
 
 struct FoodsListView: View {
   var foods: [FoodItem]
-  
+
   var body: some View {
     List(foods, id: \.self) { food in
       NavigationLink(value: food) {
@@ -21,6 +22,52 @@ struct FoodsListView: View {
   }
 }
 
-#Preview {
-  FoodsListView(foods: FoodItem.samples)
+struct FoodsListCellView: View {
+  @Environment(UserPreferences.self) private var userPreferences
+
+  let food: FoodItem
+
+  var body: some View {
+    let displayHelper = FoodDisplayHelper(food)
+
+    HStack {
+      VStack(alignment: .leading) {
+        Text(food.name)
+          .font(.headline)
+        Text(food.category ?? "")
+          .font(.subheadline)
+      }
+      Spacer()
+      Image(
+        systemName: "ellipsis.circle",
+        variableValue: displayHelper.vitaminKAsPercent(
+          of: Int(userPreferences.kTarget))
+      )
+      .symbolRenderingMode(.monochrome)
+      .foregroundColor(getImageColor())
+      .font(.title)
+    }
+    .padding(.horizontal)
+  }
+
+  func getImageColor() -> Color {
+    let displayHelper = FoodDisplayHelper(food)
+
+    switch displayHelper.vitaminKAsPercent(of: Int(userPreferences.kTarget))
+      * 100
+    {
+    case 0:
+      return .gray
+    case 1..<25:
+      return .green
+    case 25..<50:
+      return .yellow
+    case 50..<75:
+      return .orange
+    case 75...:
+      return .red
+    default:
+      return .gray
+    }
+  }
 }
