c370f634538bdc0d3c5a8efe8fb20fd257504196
Robert Tucker <robertwtucker@gmail.com>
2024-11-08 16:29:42 -0500

    feat: add handling for initial and empty search

--- KFinder/Application/Tabs/Foods/FoodsTab.swift
+++ KFinder/Application/Tabs/Foods/FoodsTab.swift
@@ -16,6 +16,7 @@ struct FoodsTab: View {
   @Bindable private var helper = FoodSearchHelper()
   @State private var searchScope = SearchScope.fdc
   @State private var isSearching = false
+  @State private var searchCount = 0
 
   private let logger = Logger(
     subsystem: Bundle.main.bundleIdentifier!,
@@ -28,6 +29,19 @@ struct FoodsTab: View {
     .overlay {
       if isSearching {
         LoadingView()
+      }
+    }
+    .overlay {
+      if searchCount == 0 {
+        ContentUnavailableView {
+          Label("foods.search.init", systemImage: "magnifyingglass")
+        } description: {
+          Text("foods.search.description")
+        }
+      } else {
+        if helper.foods.isEmpty && !isSearching {
+          ContentUnavailableView.search
+        }
       }
     }
     .searchable(text: $helper.query, prompt: "foods.search.prompt")
@@ -45,6 +59,7 @@ struct FoodsTab: View {
         logger.debug("Dispatching FDC search for '\(helper.query)'")
         Task {
           isSearching.toggle()
+          searchCount += 1
           await helper.search()
           isSearching.toggle()
         }
