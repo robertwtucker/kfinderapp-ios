ba5e469f5f58a01488e5ad33fcb44770e78b29d7
Robert Tucker <robertwtucker@gmail.com>
2024-11-11 13:32:32 -0500

    refactor: remove placeholders for personal info (#29)

--- Packages/Services/Sources/Services/UserPreferences.swift
+++ Packages/Services/Sources/Services/UserPreferences.swift
@@ -11,9 +11,6 @@ import SwiftUI
 
     @MainActor
     private class Storage {
-        @AppStorage("email", store: sharedDefaults) public var email: String = "user@kfinderapp.com"
-        @AppStorage("firstName", store: sharedDefaults) public var firstName: String = "K"
-        @AppStorage("lastName", store: sharedDefaults) public var lastName: String = "Finder"
         @AppStorage("dailyKTarget", store: sharedDefaults) public var dailyKTarget: Double = 120
         @AppStorage("setProTimeReminders", store: sharedDefaults) public var setProTimeReminders: Bool = false
         @AppStorage("defaultProTimeInterval", store: sharedDefaults) var defaultProTimeInterval: Int = 3
@@ -24,24 +21,6 @@ import SwiftUI
     public static let sharedDefaults = UserDefaults(suiteName: "group.dev.eclectic.kfinder.shared")
     public static let shared = UserPreferences()
     private let storage = Storage()
-
-    public var email: String {
-        didSet {
-            storage.email = email
-        }
-    }
-
-    public var firstName: String {
-        didSet {
-            storage.firstName = firstName
-        }
-    }
-
-    public var lastName: String {
-        didSet {
-            storage.lastName = lastName
-        }
-    }
 
     public var dailyKTarget: Double {
         didSet {
@@ -74,9 +53,6 @@ import SwiftUI
     }
 
     private init() {
-        email = storage.email
-        firstName = storage.firstName
-        lastName = storage.lastName
         dailyKTarget = storage.dailyKTarget
         setProTimeReminders = storage.setProTimeReminders
         defaultProTimeInterval = storage.defaultProTimeInterval
